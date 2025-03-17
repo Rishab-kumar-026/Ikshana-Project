35506c99c5715a4e279c29847c308f88bcf41c0d
Rishab-kumar-026 <rks2034287@gmail.com>
2025-03-17 17:13:41 +0530

    Changed Hompage

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -4,7 +4,7 @@ function HomePage() {
   return (
     <div
       style={{
-        backgroundImage: `linear-gradient(rgba(0,0,0,.2), rgba(0,0,0,.5), rgba(0,0,0,.7)), 
+        backgroundImage: `linear-gradient(rgba(0,0,0,.4), rgba(0,0,0,.7), rgba(0,0,0,.8)), 
           url("https://plus.unsplash.com/premium_photo-1663047397245-2ddad26c5dd7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")`,
         backgroundSize: "cover",
         backgroundPosition: "top 5%",
@@ -16,7 +16,7 @@ function HomePage() {
         <div className="absolute top-[20%] mx-10 text-center py-4 sm:mx-34 md:mx-48 lg:left-[-3%] lg:text-start lg:w-[40%]">
           <h1 className="text-[36px] text-zinc-200 font-bold">
             Balancing <br /> Excellence and Efficiency through <br />
-            <span className="text-green-800">Professional management...</span>
+            <span className="text-[#0cbcbd]">Professional management...</span>
           </h1>
           <h1 className="text-[36px] text-zinc-200 font-bold hidden 2xl:block mt-4">
           “Together, let’s create a facility that reflects your vision,support and your growth.”
